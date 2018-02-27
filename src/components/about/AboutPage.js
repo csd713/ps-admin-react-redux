3d60bbc6d6ab50239a31cfda73a1aa95f530a7bf
Chiranjeevi SD <jeevi146@gmail.com>
2018-02-27 11:33:14 -0500

    added basic app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About this app</h1>
+        <p> This app uses React, Redux React-Router and many other libraries</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
