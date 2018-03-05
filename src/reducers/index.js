83dbd8670ae3b838c95e9727a0f42dbd7755f8c4
Chiranjeevi SD <jeevi146@gmail.com>
2018-03-05 11:41:51 -0500

    added form to course page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
-const rootReducer = combineReducers({
-  courses
+const rootReducer = combineReducers({ //add all new reducers to this root reducer
+  courses,
+  authors
   //can use just 'courses: courses' - short hand property name
 });
 
