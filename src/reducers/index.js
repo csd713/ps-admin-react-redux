b4b482730baa3deed5114e0f16fee45ba3a1ecfc
Chiranjeevi SD <jeevi146@gmail.com>
2018-03-06 10:59:26 -0500

    handle async tasks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({ //add all new reducers to this root reducer
   courses,
-  authors
+  authors,
+  ajaxCallsInProgress
   //can use just 'courses: courses' - short hand property name
 });
 
