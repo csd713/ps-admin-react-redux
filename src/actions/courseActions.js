5ee75b854c67800cf0c49a9c2782e383aa0bd4d3
Chiranjeevi SD <jeevi146@gmail.com>
2018-03-06 11:21:02 -0500

    handle form errors

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import CourseApi from '../api/mockCourseApi';
-import { beginAjaxCall } from './ajaxStatusAction';
+import { beginAjaxCall, ajaxCallError } from './ajaxStatusAction';
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -31,6 +31,7 @@ export function saveCourse(course) {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw (error);
     });
   };
